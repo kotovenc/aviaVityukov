6c1b36cfbb02e397ab951e43047bea935c21ab4d
Lev <leosergkie@gmail.com>
2018-12-05 11:14:57 +0300

    IT IS EMPTY NOW

--- js/closeopen.js
+++
@@ -1,8 +0,0 @@
-function closeopen( id )
-    /*Для создания раскрывающихся по клику кнопочек*/
-    {
-     element = document.getElementById( id );
-          if( element )   
-             element.style.display = element.style.display == "none" ? "" : "none";   
-}
-   
