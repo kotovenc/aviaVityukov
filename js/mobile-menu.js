70f07550a10e0823ac92cc89a295599c9e0e7ce9
Krotesk <39834623+Krotesk@users.noreply.github.com>
2019-01-14 09:49:45 +0300

    5 вариантов затенения

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,4 +1,18 @@
-
+function toggleHamburger(){
+  var topBar = $(".hamburger li:nth-child(1)"),
+    middleBar = $(".hamburger li:nth-child(2)"),
+    bottomBar = $(".hamburger li:nth-child(3)");
+    if (middleBar.hasClass("rot-45deg")) {
+      topBar.removeClass("rot45deg");
+      middleBar.removeClass("rot-45deg");
+      bottomBar.removeClass("hidden");
+    } else {
+      bottomBar.addClass("hidden");
+      topBar.addClass("rot45deg");
+      middleBar.addClass("rot-45deg");
+    }
+  
+}
 $(document).ready(function() {
   //toggle menu
   $(".hamburger-container").click(function() {
@@ -13,19 +27,16 @@ $(document).ready(function() {
   });
 
   //icon animation
-  var topBar = $(".hamburger li:nth-child(1)"),
-    middleBar = $(".hamburger li:nth-child(2)"),
-    bottomBar = $(".hamburger li:nth-child(3)");
-
+  
   $(".hamburger-container").on("click", function() {
-    if (middleBar.hasClass("rot-45deg")) {
-      topBar.removeClass("rot45deg");
-      middleBar.removeClass("rot-45deg");
-      bottomBar.removeClass("hidden");
-    } else {
-      bottomBar.addClass("hidden");
-      topBar.addClass("rot45deg");
-      middleBar.addClass("rot-45deg");
-    }
+    toggleHamburger();
   });
+});
+
+ $(window).resize(function() {
+    var middleBar = $(".hamburger li:nth-child(2)");
+    if (middleBar.hasClass("rot-45deg")){
+        toggleHamburger();
+        $("#menu").slideToggle();
+    }
 });
