8770e139950c2b0db5ada923a565910019b47094
Galina <helllina99@gmail.com>
2019-01-28 11:31:31 +0300

    Обновлено до версии master. Изменены слайдеры на тренажёр Ан

--- js/menu.js
+++ js/menu.js
@@ -3,23 +3,28 @@ $( document ).ready(function() {
     if (window.innerWidth > 500) {
         $('#menu').css("background-color", "rgba(0,0,0,0)");
         $(".header-text").css("color", '#fff');
-        $(".header-text").css("backgroundColor", 'rgba(0,0,0,0)');
+        $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
     } else{
         //$('#menu').css("background-color", "rgba(255,255,255,1)");
         $(".header-text").css("color", '#000');
-        $(".header-text").css("backgroundColor", 'rgba(255,255,255,1)');
+        $(".whiteBg").css("backgroundColor", 'rgba(255,255,255,1)');
     }
 });
 
  $(window).resize(function() {
+    if(window.innerWidth < 1100 && $(window).scrollTop() > 50){
+        $(".whiteBg").css("backgroundColor", 'rgba(255,255,255,1)');
+    } else{
+        $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0');
+    }
     if (window.innerWidth > 500 && $(window).scrollTop() < 50) {
         $('#menu').css("background-color", "rgba(0,0,0,0)");
         $(".header-text").css("color", '#fff');
-        $(".header-text").css("backgroundColor", 'rgba(0,0,0,0)');
+        //$(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
     } else{
         //$('#menu').css("background-color", "rgba(255,255,255,1)");
         $(".header-text").css("color", '#000');
-        $(".header-text").css("backgroundColor", 'rgba(255,255,255,1)');
+        $(".whiteBg").css("backgroundColor", 'rgba(255,255,255,1)');
     }
 });
 
@@ -28,14 +33,27 @@ $(window).scroll(function () {
         $(".header-panel-bg").css("opacity", "1");
         if (window.innerWidth > 500) {
             $(".header-text").css("color", '#000');
-            $(".header-text").css("backgroundColor", '#fff');
+            if (window.innerWidth < 1100) {
+                $(".whiteBg").css("backgroundColor", '#fff');
+                //$(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
+            } else{
+                $(".dropdown-content").css("backgroundColor", '#fff');
+            }
+            
         }
          $('.ham-li').css("backgroundColor", '#000');
     } else
     {
         if (window.innerWidth > 500) {
          $(".header-text").css("color", '#fff');
-        $(".header-text").css("backgroundColor", 'rgba(0,0,0,0)');
+        $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
+        if (window.innerWidth < 1100) {
+                $(".whiteBg").css("backgroundColor", 'rgba(0,0,0,0)');
+                $(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
+            } else{
+                $(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
+            }
+        //$(".dropdown-content").css("backgroundColor", 'rgba(0,0,0,0)');
         }
          $(".header-panel-bg").css("opacity", "0");
           $('.ham-li').css("backgroundColor", '#fff');
